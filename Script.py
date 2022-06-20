5f4ab556f6d8591310bcb496a05b68107e5c6876
GHSrilinks4k <107419886+GHSrilinks4k@users.noreply.github.com>
2022-06-20 10:24:40 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -21,7 +21,7 @@ ABOUT_TXT = """🤖 ʙᴏᴛ ɴᴀᴍᴇ: {}
 
 📢 ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/+mi3F-bMrNQ1hMWRl>BOTS UPDATES</a>
 
-⚡ ꜱʜᴏʀᴛɴᴇʀ ; ᴅᴜʟɪɴᴋ
+
 
 💡 sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : private Repo """
    SOURCE_TXT = """<b>NOTE:</b>
@@ -105,8 +105,8 @@ This module only works for my admins
 • /unban  - <code>to unban a user.</code>
 • /channel - <code>to get list of total connected channels</code>
 • /broadcast - <code>to broadcast a message to all users</code>"""
-    
-STATUS_TXT = """📁 ᴛᴏᴛᴀʟ ꜰɪʟᴇs: <code>{}</code>
+
+     STATUS_TXT = """📁 ᴛᴏᴛᴀʟ ꜰɪʟᴇs: <code>{}</code>
 
 👤 ᴛᴏᴛᴀʟ ᴜsᴇʀs: <code>{}</code>
 
