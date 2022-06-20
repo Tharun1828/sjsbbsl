c92a0b38fcc9a44c61eef228d5d81747d3bd8fc3
GHSrilinks4k <107419886+GHSrilinks4k@users.noreply.github.com>
2022-06-20 10:56:48 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -2,35 +2,35 @@ class script(object):
 
     START_TXT = """𝙷𝙴𝙻𝙾 {},
 
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
+𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, ɪᴀᴍ ᴀ sɪᴍᴘʟᴇ ᴀᴜᴛᴏ ꜰɪʟᴛᴇʀ + ᴍᴏᴠɪᴇ sᴇᴀʀᴄʜ + ᴍᴀɴᴜᴀʟ ꜰɪʟᴛᴇʀ ʙᴏᴛ. ɪ ᴄᴀɴ ᴘʀᴏᴠɪᴅᴇ ᴍᴏᴠɪᴇs ɪɴ ᴛᴇʟᴇɢʀᴀᴍ ɢʀᴏᴜᴘs. ʏᴏᴜ ᴄᴀɴ sᴇᴀʀᴄʜ ᴍᴏᴠɪᴇs ᴠɪᴀ ɪɴʟɪɴᴇ. ɪ ᴄᴀɴ ᴀʟsᴏ ᴀᴅᴅ ꜰɪʟᴛᴇʀs ɪɴ ᴛᴇʟᴇɢʀᴀᴍ ɢʀᴏᴜᴘs. ᴊᴜsᴛ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴀɴᴅ ᴇɴᴊᴏʏ😌"""
 
     HELP_TXT = """𝙷𝙴𝚈 {}
 
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
 
-    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
+    ABOUT_TXT = """🤖 ʙᴏᴛ ɴᴀᴍᴇ: {}
 
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>
+👨‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href=https://t.me/Sridhar_sm18>Sridhar Sm18</a>
 
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
+📚 ꜰʀᴀᴍᴇᴡᴏʀᴋ : 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
+📝 ʟᴀɴɢᴜᴀɢᴇ : 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
+📡 ʜᴏsᴛᴇᴅ ᴏɴ : ʜᴇʀᴏᴋᴜ
 
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
+👥 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ : 
 
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
+📢 ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ : 
 
     SOURCE_TXT = """<b>NOTE:</b>
 
-- Eva Maria is a open source project. 
+- Bot Was Not open source project. 
 
-- Source - https://github.com/EvamariaTG/EvaMaria  
+- Source - https://t.me/srilinks4k
 
 <b>DEVS:</b>
 
-- <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>"""
+- <a href=https://t.me/Sridhar_Sm18>SRIDHAR SM18</a>"""
 
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
@@ -38,7 +38,7 @@ class script(object):
 
 <b>NOTE:</b>
 
-1. eva maria should have admin privillage.
+1. Bot should have admin privillage.
 
 2. only admins can add filters in a chat.
 
@@ -56,19 +56,19 @@ class script(object):
 
     BUTTON_TXT = """Help: <b>Buttons</b>
 
-- Eva Maria Supports both url and alert inline buttons.
+- Bot Supports both url and alert inline buttons.
 
 <b>NOTE:</b>
 
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
 
-2. Eva Maria supports buttons with any telegram media type.
+2. Bot supports buttons with any telegram media type.
 
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
 
-<code>[Button Text](buttonurl:https://t.me/EvaMariaBot)</code>
+<code>[Button Text](buttonurl:https://t.me/sm18filterbot)</code>
 
 <b>Alert buttons:</b>
 
@@ -110,7 +110,7 @@ class script(object):
 
 <b>NOTE:</b>
 
-these are the extra features of Eva Maria
+these are the extra features of SRILINKS4K
 
 <b>Commands and Usage:</b>
 
@@ -152,15 +152,15 @@ This module only works for my admins
 
 • /broadcast - <code>to broadcast a message to all users</code>"""
 
-    STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
+    STATUS_TXT = """📁 ᴛᴏᴛᴀʟ ꜰɪʟᴇs: <code>{}</code>
 
-★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
+👤 ᴛᴏᴛᴀʟ ᴜsᴇʀs : <code>{}</code>
 
-★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
+👥 ᴛᴏᴛᴀʟ ᴄʜᴀᴛs : <code>{}</code>
 
-★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
+⚙️ ᴜsᴇᴅ sᴛᴏʀᴀɢᴇ : <code>{}</code> 𝙼𝚒𝙱
 
-★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
+🆓 ꜰʀᴇᴇ sᴛᴏʀᴀɢᴇ : <code>{}</code> 𝙼𝚒𝙱"""
 
     LOG_TEXT_G = """#NewGroup
 
