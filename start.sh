799a945c8d3990c2bcc7c4434251a98568e8edad
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-07-02 23:39:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/GHSrilinks4k/Sm18filter4k
+  git clone https://github.com/GHSrilinks4k/Sm18filter4k.git /Sm18filter4k
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone https://github.com/GHSrilinks4k/Sm18filter4k
