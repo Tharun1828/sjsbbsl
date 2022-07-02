6f3cfa54bbfe2c040d9556f73d7136ddde97d01e
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-07-02 23:43:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,8 +6,10 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone https://github.com/GHSrilinks4k/Sm18filter4k
 fi
+ls
 cd /Sm18filter4k
 ls
+$PWD
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
