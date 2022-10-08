6e46017a220165de65d35bf29b8bdb23a377bdd3
MS <108895850+Saifudeebot@users.noreply.github.com>
2022-10-08 14:06:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -97,7 +97,7 @@ async def next_page(bot, query):
     # How to Download button
 
     btn.append(
-    [InlineKeyboardButton("HOW TO DOWNLOAD 🤔", url="https://t.me/TAMIL_MOVIES_UPDATE_ms/122"),]
+    [InlineKeyboardButton("HOW TO DOWNLOAD 🤔", url="https://t.me/TAMIL_MOVIES_UPDATE_ms/322"),]
 )
 
     if n_offset == 0:
@@ -682,7 +682,7 @@ async def auto_filter(client, msg, spoll=False):
         ]
 
     btn.append(
-    [InlineKeyboardButton("How to download 🤔", url="https://t.me/TAMIL_MOVIES_UPDATE_ms/122"),]
+    [InlineKeyboardButton("How to download 🤔", url="https://t.me/TAMIL_MOVIES_UPDATE_ms/322"),]
 )
 
     if offset != "":
