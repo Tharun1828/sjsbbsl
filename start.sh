5bc36cd6a15721a6cf75b735e1ba37f6d8176cb9
DeveloperTharun <114932079+DeveloperTharun@users.noreply.github.com>
2022-10-11 21:13:22 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/DeveloperTharun/sjsbbsl.git /EvaMaria
+  git clone https://github.com/Saifudeebot/Sm18filter4kdu.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
